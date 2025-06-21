8c4955698d803edea522a307035c59b89a0bde1b
Kryar <57811578+Rellex2019@users.noreply.github.com>
2025-06-21 12:24:38 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
         port: 5173,
         strictPort: true,
         hmr: {
-            host: 'localhost',  // Для Docker используйте localhost
+            host: '87.228.101.27',  // Для Docker используйте localhost
             port: 5173,
         },
         watch: {
